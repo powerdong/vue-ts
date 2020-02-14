21724ff40b0d8673a2e69bc17dab2198a87d6dfa
Lambda <m19893170798@163.com>
2020-02-14 16:34:11 +0800

    Add files via upload

---
+++ ts-demo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  width: 300px;
+  margin: 0 auto;
+}
+#nav{
+  width: 200px;
+  height: 30px;
+  font-size: 20px;
+  font-weight: 600
+}
+</style>
---
+++ ts-demo/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    这是一个子组件的HelloWorld
+    Msg: {{msg}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private readonly msg !: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
